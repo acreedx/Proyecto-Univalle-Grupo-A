8c20681966597ba2850bb227b7b266828bfd8be4
LeoGMR20 <leo250703@gmail.com>
2023-09-25 13:18:32 -0400

    Commit 25/09/2023 13:18 (Leandro Morales) ->Cambios en card gray y corrección de error de children props en card gray

--- src/app/components/card-gray.tsx
+++ src/app/components/card-gray.tsx
@@ -9,14 +9,15 @@ const CardGray: FC<cardProps> = ({ title, children }) => {
   return (
     <div className="flex items-center min-h-min justify-center h-80 p-6 bg-gray-500 border border-gray-500 rounded-xl drop-shadow-lg shadow dark:bg-gray-500 dark:border-gray-500">
       <div className="text-center">
-        <h5 className="mb-2 text-4xl font-bold tracking-tight text-white dark:text-white">
+        <h5 className="mb-2 font-bold tracking-tight text-white dark:text-white text-lg min-[320px]:text-xl sm:text2xl md:text-3xl">
           {title}
         </h5>
-        <div className="text-2xl text-white">{children}</div>
+        <div className=" text-white text-base min-[320px]:text-lg sm:text-xl md:text-2xl">
+          {children}
+        </div>
       </div>
     </div>
   );
 };
 
 export default CardGray;
-
--- src/app/saludBienestar/gabinetePsicoPedagogico/page.tsx
+++ src/app/saludBienestar/gabinetePsicoPedagogico/page.tsx
@@ -1,6 +1,6 @@
 import Circularbutton from "../../components/circular-button";
-import Card from "../../components/card-gray"
-import style from "./page.module.css"
+import CardGray from "../../components/card-gray";
+import style from "./page.module.css";
 import VideoPlayer from "@/app/components/video-player";
 import HeaderTitle from "@/app/components/header-title";
 
@@ -16,55 +16,70 @@ function GabinetePsicoPedagogicoPage() {
         title="Gabinete Psico-Pedagogico"
       />
       <div className="flex flex-row justify-center gap-10 mx-3">
-          <div className="basis-5/12" >
-            <VideoPlayer url={videoUrl} width={videoWidth} height={videoHeight} />
-          </div>
-          <div className="basis-1/4">
-            <Card title={"Contactos"} 
-                  children={
-                  <ul>
-                    <li>
-                      Teléfonos:
-                      <ul>
-                        <li>(591-2) 2001800</li>
-                        <li>(591-2) 2246725</li>
-                        <li>(591-2) 2246726</li>
-                      </ul>
-                    </li>
-                    <li>
-                      Whatsapp:
-                      <ul>
-                        <li>+591 77277872</li>
-                      </ul>
-                    </li>
-                  </ul>}
-            />
-          </div>
-          <div className="basis-1/5">
-            <Card title={"Horarios de atención"} 
-                  children={
-                  <ul>
-                    <li>Lunes a Viernes: 08:00 a 19:00</li>
-                    <li>Sábado: 08:00 a 12:00</li>
-                  </ul>}
-            />
-          </div>
-      </div>
-      <h1 className="text-center mt-10 text-3xl font-bold text-white">Requisitos para usar el gabinete Psico-pedagógico </h1>
-    <div className={style.description}>
-      <div className={style["description-item"]}>
-        <h5 className="mb-2 text-3xl font-bold tracking-tight text-white dark:text-white text-center">Requisito 1</h5>
-        <p className="font-normal text-lg text-white dark:text-white text-center" >Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+        <div className="basis-5/12">
+          <VideoPlayer url={videoUrl} width={videoWidth} height={videoHeight} />
         </div>
-      <div className={style["description-item"]}>
-      <h5 className="mb-2 text-3xl font-bold tracking-tight text-white dark:text-white text-center">Requisito 2</h5>
-      <p className="font-normal text-lg text-white dark:text-white text-center" >Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+        <div className="basis-1/4">
+          <CardGray title={"Contactos"}>
+            <ul>
+              <li>
+                Teléfonos:
+                <ul>
+                  <li>(591-2) 2001800</li>
+                  <li>(591-2) 2246725</li>
+                  <li>(591-2) 2246726</li>
+                </ul>
+              </li>
+              <li>
+                Whatsapp:
+                <ul>
+                  <li>+591 77277872</li>
+                </ul>
+              </li>
+            </ul>
+          </CardGray>
+        </div>
+        <div className="basis-1/5">
+          <CardGray title={"Horarios de atención"}>
+            <ul>
+              <li>Lunes a Viernes: 08:00 a 19:00</li>
+              <li>Sábado: 08:00 a 12:00</li>
+            </ul>
+          </CardGray>
+        </div>
       </div>
-      <div className={style["description-item"]}>
-      <h5 className="mb-2 text-3xl font-bold tracking-tight text-white dark:text-white text-center">Requisito 3</h5>
-        <p className="font-normal text-lg text-white dark:text-white text-center" >Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+      <h1 className="text-center mt-10 text-3xl font-bold text-white">
+        Requisitos para usar el gabinete Psico-pedagógico{" "}
+      </h1>
+      <div className={style.description}>
+        <div className={style["description-item"]}>
+          <h5 className="mb-2 text-3xl font-bold tracking-tight text-white dark:text-white text-center">
+            Requisito 1
+          </h5>
+          <p className="font-normal text-lg text-white dark:text-white text-center">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.{" "}
+          </p>
+        </div>
+        <div className={style["description-item"]}>
+          <h5 className="mb-2 text-3xl font-bold tracking-tight text-white dark:text-white text-center">
+            Requisito 2
+          </h5>
+          <p className="font-normal text-lg text-white dark:text-white text-center">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.{" "}
+          </p>
+        </div>
+        <div className={style["description-item"]}>
+          <h5 className="mb-2 text-3xl font-bold tracking-tight text-white dark:text-white text-center">
+            Requisito 3
+          </h5>
+          <p className="font-normal text-lg text-white dark:text-white text-center">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.{" "}
+          </p>
+        </div>
       </div>
-    </div>
     </main>
   );
 }
