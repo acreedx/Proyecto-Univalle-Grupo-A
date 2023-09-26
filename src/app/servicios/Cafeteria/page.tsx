684935210d614ba7b489a75f54831590ff79771c
Bizarr420 <jt094593@gmail.com>
2023-09-26 18:42:15 -0400

    Actualizacion de cafeteria

--- src/app/servicios/Cafeteria/page.tsx
+++ src/app/servicios/Cafeteria/page.tsx
@@ -13,7 +13,7 @@ function CafeteriaPage() {
           <div className="flex-1  p-4">           
             <h1 className="text-center mt-10 text-4xl font-bold text-white">Video informativo</h1>
             <center className="p-10">
-            <iframe width="480" height="360" src="https://www.youtube.com/embed/iA9vIfTF7Wo" title="Vida del programador - Gracioso" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"></iframe>
+            <iframe width="480" height="360" src="https://www.youtube.com/embed/9OX7cGzFoJw" title="video" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"></iframe>
             </center>
             <div className="rounded-3xl bg-gray-400 text-white p-4 text-2xl mx-20">
               <h1 className="text-center font-bold text-white text-4xl p-5">Horario de atencion</h1>
