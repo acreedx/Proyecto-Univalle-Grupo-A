741f9a7aa77d4e423fdbc018e9662e20303a6f63
LeoGMR20 <leo250703@gmail.com>
2023-09-28 16:09:36 -0400

    Commit 28/09/2023 (Leandro Morales) -> Cambios en la pantalla obj perdidos

--- src/app/saludBienestar/bienestarUniversitario/objetosPerdidos/page.tsx
+++ src/app/saludBienestar/bienestarUniversitario/objetosPerdidos/page.tsx
@@ -1,5 +1,51 @@
+"use client";
+
+import { useState, useEffect } from "react";
+import ItemMenu from "@/app/components/item-menu";
+import ButtonNav from "@/app/components/ButtonNav";
+import menu from "@/app/DataTools/DataMenuCafeteria";
+import HeaderTitle from "@/app/components/header-title";
+
 function ObjetosPerdidosPage() {
-  return <div>page</div>;
+  const [missObjs, setMissObjs] = useState([]);
+
+  //Get Information
+
+  useEffect(() => {
+    getInfo();
+  }, []);
+
+  const getInfo = () => {
+    fetch("https://jsonplaceholder.typicode.com/photos")
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        setMissObjs(data);
+      })
+      .catch(() => console.log("error"));
+  };
+
+  return (
+    <div className="h-full grid gap-5">
+      <HeaderTitle
+        direction="/saludBienestar/bienestarUniversitario"
+        title="Objetos Perdidos"
+      />
+
+      <div className="flex flex-wrap justify-center items-center gap-4">
+        {missObjs.map((obj, index) => (
+          <ItemMenu
+            key={index}
+            imageUrl="{obj.imageUrl}"
+            name="{obj.title}"
+            price="a"
+            description="a"
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default ObjetosPerdidosPage;
