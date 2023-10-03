56d0ab6ea5b885b3c272ae45f083e5a1632b2bf0
Drazen27 <103655456+Drazen27@users.noreply.github.com>
2023-10-02 21:49:38 -0400

    Commit del pdf viewwer

--- src/app/saludBienestar/bienestarUniversitario/becasAyudas/page.tsx
+++ src/app/saludBienestar/bienestarUniversitario/becasAyudas/page.tsx
@@ -1,5 +1,15 @@
+import HeaderTitle from "@/app/components/header-title";
+import PDFViewer from "@/app/components/pdfViewer";
+
 function BecasAyudasPage() {
-  return <div>page</div>;
+  return(
+  <div>
+    <div className=" mb-5 ">
+    <HeaderTitle title="Reglamento de Becas" direction="/saludBienestar/bienestarUniversitario"/>
+    </div>
+    <PDFViewer href="https://pdftron.s3.amazonaws.com/downloads/pl/demo-annotated.pdf"/>
+  </div>
+  );
 }
 
 export default BecasAyudasPage;
