a0a23d453f01fdeb2ba0a4d8d7a5653cfd132ea3
dani3571 <ccd2024081@est.univalle.edu>
2023-11-16 12:22:53 -0400

    Added locations provider

--- src/app/administracion/tramites/[id]/page.tsx
+++ src/app/administracion/tramites/[id]/page.tsx
@@ -2,27 +2,21 @@
 import { useEffect, useState } from 'react'
 import ImageCarrousel from "@/app/components/image-carrousel";
 import HeaderTitle from '@/app/components/header-title';
-import { GetServerSidePropsContext } from 'next';
+
 import URL from '@/../utils/api';
 import { ITramitesData, convertJSONService } from "@/../utils/interfaces/tramitesData";
 import { IStepRequirementData } from '@/../utils/interfaces/stepRequerimentData';
 import stepRequerimentProvider from '@/../utils/providers/stepRequerimentProvider';
-
-const slides = [
-  "/administracion/tramites.png",
-  "/administracion/cajas.png",
-];
-const descriptions = [
-  "Tramites se encuentra en el primer piso de la torre innovacion al lado de cajas",
-  "Cajas se encuentran en el primer piso de la torre innovacion al lado de la ventanilla de tramites",
-];
+import locationsProvider from '../../../../../utils/providers/locationsProvider';
 
 function InformacionTramite({ params }: { params: { id: number } }) {
   const [service, setService] = useState<ITramitesData>();
   const route = "Servicios/getTramiteById/";
   const { id } = params;
   const [stepsRequirements, setStepsrequirements] = useState<IStepRequirementData[]>([]);
-
+  
+  const [slides, setSlides] = useState<string[]>([]);
+  const [descriptions, setDescriptions] = useState<string[]>([]);
   useEffect(() => {
     async function doFetch() {
       try {
@@ -35,6 +29,13 @@ function InformacionTramite({ params }: { params: { id: number } }) {
           });
 
         setStepsrequirements(await stepRequerimentProvider.GetStepsRequirementsList(id))
+        const locations = await locationsProvider.GetLocationsList(id)
+        const newSlides = locations.map(item => item.imagen)
+        const newDescriptions = locations.map(item => item.descripcion)
+        setSlides(newSlides)
+        setDescriptions(newDescriptions)
+       
+       
       } catch {
 
       }
@@ -50,21 +51,24 @@ function InformacionTramite({ params }: { params: { id: number } }) {
 
       <div className="grid grid-cols-1 mt-1 p-5 md:p-10 gap-5 md:gap-20 py-0 xl:grid-cols-12">
         <div className="col-span-7 xl:col-span-5 md:col-span-10">
-          <ImageCarrousel
-            slides={slides}
-            description={descriptions}
-          />
+              <ImageCarrousel
+              slides={slides}
+              description={descriptions}
+            />
+
           <div className="mt-8"></div>
           <div className="flex flex-row items-center gap-4 justify-center">
             <img
               src="/icons/usericon.png"
               alt="Encargado icon"
-              className="w-12"
+              className="w-14"
             />
-            <p className="text-white text-base md:text-xl font-bold">
+            <p className="text-white text-base md:text-2xl font-bold">
               {service?.encharged}
             </p>
+            
           </div>
+        
         </div>
         <div className="col-span-7 text-white font-bold m-5 md:m-10 md:col-span-10 xl:col-span-7 relative">
           <h1 className="mb-5 text-2xl md:text-3xl text-center">Requisitos</h1>
@@ -75,24 +79,22 @@ function InformacionTramite({ params }: { params: { id: number } }) {
                 {e.description}
 
 
-                  {e.pasosRequisito.map((paso, pasoIndex) => (
-                    <ol key={pasoIndex} className='ml-20 text-lg md:text-2xl text-justify list-disc grid gap-2 md:gap-5'>
-                        <li>{paso.nameStep}</li>  
-                    </ol>
-                  ))}
-
-            
-
+                {e.pasosRequisito.map((paso, pasoIndex) => (
+                  <ol key={pasoIndex} className='ml-20 text-lg md:text-2xl text-justify list-disc grid gap-2 md:gap-5'>
+                    <li>{paso.nameStep}</li>
+                  </ol>
+                ))}
               </li>
             })}
           </ol>
-          <div className="absolute right-2 md:-bottom-10 md:right-5 flex flex-row items-center gap-2 bg-slate-600 rounded-2xl p-1 md:p-2">
+          <div className="flex justify-center items-center gap-4 bg-slate-600 rounded-2xl 
+          p-1 md:p-3 mt-14 ml-96">
             <img
               src="/icons/clockicon.png"
               alt="Encargado icon"
-              className="w-8 md:w-10"
+              className="w-8 md:w-14"
             />
-            <p className="text-white text-sm md:text-base font-bold">
+            <p className="text-white md:text-xl font-bold"> 
               Duración del trámite: {service?.duration}
             </p>
           </div>
